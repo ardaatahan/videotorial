419eef588e86d59317bddd80f445ecfbac2370db
Mert Şen <m.sen@ug.bilkent.edu.tr>
2023-05-31 18:25:32 +0300

    server.js ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -57,7 +57,7 @@ app.post('/upload', upload.single('video'), async (req, res) => {
   }
 });
 
-// Start the server
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
 });
