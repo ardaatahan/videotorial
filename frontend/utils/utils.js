64e55853dbf220cdaa50c646f05f07bc41f05b2c
ardaatahan <63500745+ardaatahan@users.noreply.github.com>
2023-05-31 18:45:40 +0300

    change coloring of object tracking and remove unecessary functions and code

--- frontend/utils/utils.js
+++ frontend/utils/utils.js
@@ -1,31 +1,31 @@
-function draw_bounding_boxes(object_tracks, ctx) {
-    ctx.clearRect(0, 0, 800, 500)
+// function draw_bounding_boxes(object_tracks, ctx) {
+//     ctx.clearRect(0, 0, 800, 500)
 
-    const current_time = video.currentTime
+//     const current_time = video.currentTime
 
-    object_tracks.forEach(tracked_object => {
+//     object_tracks.forEach(tracked_object => {
 
-        if (tracked_object.has_frames_for_time(current_time)) {
-            draw_bounding_box(tracked_object.current_bounding_box(current_time), tracked_object.name, ctx)
-        }
+//         if (tracked_object.has_frames_for_time(current_time)) {
+//             draw_bounding_box(tracked_object.current_bounding_box(current_time), tracked_object.name, ctx)
+//         }
 
-    })
-}
+//     })
+// }
 
-function draw_bounding_box(box, name = null, ctx) {
-    ctx.strokeStyle = "#4285F4"
-    ctx.beginPath()
-    ctx.lineWidth = 3
-    ctx.rect(box.x, box.y, box.width, box.height)
-    ctx.stroke()
+// function draw_bounding_box(box, name = null, ctx) {
+//     ctx.strokeStyle = "#4285F4"
+//     ctx.beginPath()
+//     ctx.lineWidth = 3
+//     ctx.rect(box.x, box.y, box.width, box.height)
+//     ctx.stroke()
 
-    if (name) {
-        ctx.fillStyle = "#4285F4"
-        ctx.fillRect(box.x, box.y, name.length * 13, 32)
-        ctx.fillStyle = "#ffffff"
-        ctx.fillText(name, box.x + 5, box.y + 22)
-    }
-}
+//     if (name) {
+//         ctx.fillStyle = "#4285F4"
+//         ctx.fillRect(box.x, box.y, name.length * 13, 32)
+//         ctx.fillStyle = "#ffffff"
+//         ctx.fillText(name, box.x + 5, box.y + 22)
+//     }
+// }
 
 function nullable_time_offset_to_seconds(time_offset) {
     if (!time_offset)
